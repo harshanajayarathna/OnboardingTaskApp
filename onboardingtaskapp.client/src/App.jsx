087cf53a9505d52ffb25750e1705daee63c89a2e
Harshana <harshanajayarathna@gmail.com>
2024-04-25 12:46:17 +1200

    Cleared App.jsx file

--- onboardingtaskapp.client/src/App.jsx
+++ onboardingtaskapp.client/src/App.jsx
@@ -5,17 +5,9 @@ import './App.css';
 
 import Header from './components/Header/Header';
 import CustomerList from './components/Customer/List';
-import CustomerCreate from './components/Customer/Create';
-import CustomerEdit from './components/Customer/Edit';
 import ProductList from './components/Product/List';
-import ProductCreate from './components/Product/Create';
-import ProductEdit from './components/Product/Edit';
 import StoreList from './components/Store/List';
-import StoreCreate from './components/Store/Create';
-import StoreEdit from './components/Store/Edit';
 import SaleList from './components/Sale/List';
-import SaleCreate from './components/Sale/Create';
-import SaleEdit from './components/Sale/Edit';
 
 function App() {   
     return (
@@ -27,8 +19,6 @@ function App() {
                     <Route path="/products" element={<ProductList />} />
                     <Route path="/stores" element={<StoreList />} />
                     <Route path="/sales" element={<SaleList />} />
-                                        
-
                 </Routes>
             </BrowserRouter>
         </>
